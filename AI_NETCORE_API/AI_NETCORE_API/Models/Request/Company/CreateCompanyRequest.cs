f7327c3304d90b3704cd9b7ec7907f5dbad3bb2e
Maciej Cebula <maciejcebula96@gmail.com>
2019-11-06 20:53:19 +0100

    Bad swagger response - fix Execution details have to be nullable, databaseExecution is not required

---
+++ AI_NETCORE_API/AI_NETCORE_API/Models/Request/Company/CreateCompanyRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AI_NETCORE_API.Models.Request.Company
+{
+    public class CreateCompanyRequest
+    {
+        public string Name { get; set; }
+        public int ResourceAmount { get; set; }
+
+
+        public bool IsValid => !string.IsNullOrWhiteSpace(Name) && ResourceAmount > 0;
+    }
+}
---
+++ AI_NETCORE_API/AI_NETCORE_API/Models/Response/Companies/CreateCompanyResponseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AI_NETCORE_API.Models.Response.ExecutingTimes;
+
+namespace AI_NETCORE_API.Models.Response.Companies
+{
+    public class CreateCompanyResponseModel
+    {
+        public ExecutionDetails ExecutionDetails { get; set; }
+    }
+}
