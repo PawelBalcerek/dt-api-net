930ffda527313f6131d4d0635cf0223f1b723311
AzuxDario <pawel_mibor95@tlen.pl>
2019-11-09 14:14:18 +0100

    Updated database model

--- AI_NETCORE_API/Domain/BusinessObject/SellOffer.cs
+++ AI_NETCORE_API/Domain/BusinessObject/SellOffer.cs
@@ -6,10 +6,12 @@ namespace Domain.BusinessObject
 {
     public class SellOffer : BaseOffer
     {
-        public SellOffer(int id, int resourceId, int amount, DateTime date, bool isValid,decimal price) : base(id,resourceId, amount, date, isValid)
+        public SellOffer(int id, int resourceId, int amount, DateTime date, bool isValid, decimal price, int startamount) : base(id,resourceId, amount, date, isValid)
         {
             Price = price;
+            StartAmount = startamount;
         }
         public decimal Price { get; }
+        public int StartAmount { get; }
     }
 }
