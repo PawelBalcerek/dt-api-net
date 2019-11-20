48cfecfb92d161bf31d066b5c0dafb18c33d5e2d
AzuxDario <pawel_mibor95@tlen.pl>
2019-11-20 12:40:34 +0100

    Fix

--- AI_NETCORE_API/Domain/Creators/SellOffer/Request/Concrete/SellOfferCreateRequest.cs
+++ AI_NETCORE_API/Domain/Creators/SellOffer/Request/Concrete/SellOfferCreateRequest.cs
@@ -7,16 +7,18 @@ namespace Domain.Creators.SellOffer.Request.Concrete
 {
     public class SellOfferCreateRequest : ISellOfferCreateRequest
     {
-        public SellOfferCreateRequest(int resourceId, int amount, double price)
+        public SellOfferCreateRequest(int resourceId, int amount, double price, int userId)
         {
             ResourceId = resourceId;
             Amount = amount;
             Price = price;
+            UserId = userId;
         }
 
         public int ResourceId { get; }
         public int Amount { get; }
         public double Price { get; }
+        public int UserId { get; }
     }
  
 }
